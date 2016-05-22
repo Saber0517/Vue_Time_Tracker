aa6e13612f2a081d7c4e4862bfc554ed7dd002eb
Saber0517 <zzbond0517@gmail.com>
2016-05-22 11:19:01 +0800

    init use router

--- vue-time-tracker/src/main.js
+++ vue-time-tracker/src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App'
+import Hello from './components/Hello.vue'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+// We want to apply VueResource and VueRouter
+// to our Vue instance
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const router = new VueRouter()
+// Pointing routes to the components they should use
+
+router.map({
+  '/hello': {
+    component: Hello
+  }
 })
+
+// Any invalid route will redirect to home
+router.redirect({
+  '*': '/hello'
+})
+
+router.start(App, '#app')
